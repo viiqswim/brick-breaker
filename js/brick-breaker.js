8f9a6696d27b442170a938629542f061c2c82921
Victor Dozal <v.dozal@live.com>
2017-05-05 21:13:58 -0500

    Added game assets

--- js/brick-breaker.js
+++ js/brick-breaker.js
@@ -1,7 +1,8 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
-
+    game.load.atlas('breakout', 'assets/games/breakout/breakout.png', 'assets/games/breakout/breakout.json');
+    game.load.image('starfield', 'assets/misc/starfield.jpg');
 }
 
 function create() {
